f82fe9c1da36f3b18fd8dfd2bcac5c891bc93328
will1001 <wilirahmatm@gmail.com>
2023-05-14 19:44:55 +0800

    fix - warning and bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,12 +22,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 import { Navbar } from "@/components/Navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <div className="flex justify-center">
-        <img className="absolute h-[100vh]" src="./radial_efect.png" alt="" />
-        <img className="absolute h-[100vh]" src="./curveline.png" alt="" />
+        <Image
+          className="absolute h-[100vh]"
+          src="/radial_efect.png"
+          alt=""
+          fill
+        />
+        <Image
+          className="absolute h-[100vh]"
+          src="/curveline.png"
+          alt=""
+          fill
+        />
         <div className="grid grid-cols-2 ">
           <div className="flex items-center justify-center ">
             <div className="text-4xl w-[420px] space-y-6 z-10">
               <span>Hai! </span>
-              <span className="font-bold">I'm Wili</span>
+              <span className="font-bold">Im Wili</span>
               <br />
               <span className="font-bold">Full Stack </span>
               <span>Developer </span>
@@ -22,12 +33,20 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="flex flex-col items-center justify-center  relative">
-            <img className="h-[500px]" src="./circle.png" alt="" />
-            <img
-              className="absolute left-45 h-[500px]"
-              src="./wili.png"
+          <div className="flex flex-col items-end justify-end  relative">
+            <Image
+              className="h-[500px]"
+              src="/circle.png"
               alt=""
+              width="440"
+              height="500"
+            />
+            <Image
+              className="absolute left-10 h-[500px]"
+              src="/wili.png"
+              alt=""
+              width="320"
+              height="200"
             />
           </div>
         </div>
