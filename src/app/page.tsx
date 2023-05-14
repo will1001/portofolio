5490c2145416ef9a1f4e9663c38ed0fb77fe0c38
will1001 <wilirahmatm@gmail.com>
2023-05-14 19:27:49 +0800

    feat - create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,34 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <div className="flex justify-center">
+        <img className="absolute h-[100vh]" src="./radial_efect.png" alt="" />
+        <img className="absolute h-[100vh]" src="./curveline.png" alt="" />
+        <div className="grid grid-cols-2 ">
+          <div className="flex items-center justify-center ">
+            <div className="text-4xl w-[420px] space-y-6 z-10">
+              <span>Hai! </span>
+              <span className="font-bold">I'm Wili</span>
+              <br />
+              <span className="font-bold">Full Stack </span>
+              <span>Developer </span>
+              <br />
+              <p className="text-sm">
+                Full-stack developer. For the past 6 years, I have been working
+                with many startups in Indonesia to develop great software.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center  relative">
+            <img className="h-[500px]" src="./circle.png" alt="" />
+            <img
+              className="absolute left-45 h-[500px]"
+              src="./wili.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <div className="flex justify-center">
-      <span>Home</span>
-      <span>Projects</span>
-      <span>Blog</span>
-      <span>About</span>
-      <span>Contact</span>
+    <div className="flex justify-center space-x-10 p-5">
+      <span className="cursor-pointer">Home</span>
+      <span className="cursor-pointer">Projects</span>
+      <span className="cursor-pointer">Blog</span>
+      <span className="cursor-pointer">About</span>
+      <span className="cursor-pointer">Contact</span>
     </div>
   );
 };
