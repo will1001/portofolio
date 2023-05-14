51cb53fff7be478a65efdebb5d6e377093264b93
will1001 <wilirahmatm@gmail.com>
2023-05-14 16:52:13 +0800

    initial setup project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-center">
+      <span>Home</span>
+      <span>Projects</span>
+      <span>Blog</span>
+      <span>About</span>
+      <span>Contact</span>
+    </div>
+  );
+};
