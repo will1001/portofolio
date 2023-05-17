781d223a8b5967e504cdb5b79bd3de68f840deb7
will1001 <wilirahmatm@gmail.com>
2023-05-17 08:26:08 +0800

    update navbar link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,22 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <div className="flex justify-center xs:space-x-5 md:space-x-10 p-5">
-      <span className="cursor-pointer">Home</span>
-      <span className="cursor-pointer">Projects</span>
-      <span className="cursor-pointer">Blog</span>
-      <span className="cursor-pointer">About</span>
-      <span className="cursor-pointer">Hire Me</span>
+    <div className="flex justify-center items-center xs:space-x-5 md:space-x-10 p-5">
+      <span className="cursor-pointer hover:text-[#7DE0EA] transition duration-300 ease-in-out">
+        Home
+      </span>
+      <span className="cursor-pointer hover:text-[#7DE0EA] transition duration-300 ease-in-out">
+        Projects
+      </span>
+      <a href="https://blog.wilirahmatm.com/" className="cursor-pointer hover:text-[#7DE0EA] transition duration-300 ease-in-out">
+        Blog
+      </a>
+      {/* <span className="cursor-pointer hover:text-[#7DE0EA] transition duration-300 ease-in-out">
+        About
+      </span> */}
+      <a href="https://www.upwork.com/freelancers/~010e9458891ddd920f" className="cursor-pointer bg-[#7DE0EA] text-white rounded-lg py-2 px-5">
+        <span>Hire Me &#8594;</span>
+      </a>
     </div>
   );
 };
